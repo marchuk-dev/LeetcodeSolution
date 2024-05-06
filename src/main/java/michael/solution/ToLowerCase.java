f6b5c5b5a5477fcdb75f976a50f36b4e92546c1f
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-05-06 23:46:21 +0300

    https://leetcode.com/submissions/detail/1251207662/

--- src/main/java/michael/solution/ToLowerCase.java
+++ src/main/java/michael/solution/ToLowerCase.java
@@ -1,2 +1,11 @@
-package michael.solution;public class ToLowerCase {
+package michael.solution;
+
+// https://leetcode.com/problems/to-lower-case/description/?envType=study-plan-v2&envId=programming-skills
+public class ToLowerCase {
+    public String toLowerCase(String s) {
+        char[] charArray = s.toCharArray();
+        for (int i = 0; i < charArray.length; i++)
+            if (Character.isUpperCase(charArray[i])) charArray[i] = Character.toLowerCase(charArray[i]);
+        return String.valueOf(charArray);
+    }
 }
