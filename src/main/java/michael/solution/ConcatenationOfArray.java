2fdc169bd9db2a81971650af413fe766e62cb3ba
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-07-03 22:55:31 +0300

    done (#27)

---
+++ src/main/java/michael/solution/ConcatenationOfArray.java
@@ -0,0 +1,11 @@
+package michael.solution;
+
+// https://leetcode.com/problems/concatenation-of-array/
+class ConcatenationOfArray {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length * 2];
+        System.arraycopy(nums, 0, ans, 0, nums.length);
+        System.arraycopy(nums, 0, ans, nums.length, nums.length);
+        return ans;
+    }
+}
